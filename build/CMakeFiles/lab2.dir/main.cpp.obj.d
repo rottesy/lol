39b0e5e3803732f6bb65bd8ca1ff02343b4c7c7d
Rottesy1 <egorkozel999@gmail.com>
2025-09-14 18:15:42 +0300

    4

---
+++ build/CMakeFiles/lab2.dir/main.cpp.obj.d
@@ -0,0 +1,136 @@
+CMakeFiles/lab2.dir/main.cpp.obj: D:\lab2\main.cpp \
+ D:/msy2/ucrt64/include/c++/15.2.0/iostream \
+ D:/msy2/ucrt64/include/c++/15.2.0/bits/requires_hosted.h \
+ D:/msy2/ucrt64/include/c++/15.2.0/x86_64-w64-mingw32/bits/c++config.h \
+ D:/msy2/ucrt64/include/c++/15.2.0/x86_64-w64-mingw32/bits/os_defines.h \
+ D:/msy2/ucrt64/include/c++/15.2.0/x86_64-w64-mingw32/bits/cpu_defines.h \
+ D:/msy2/ucrt64/include/c++/15.2.0/pstl/pstl_config.h \
+ D:/msy2/ucrt64/include/c++/15.2.0/ostream \
+ D:/msy2/ucrt64/include/c++/15.2.0/bits/ostream.h \
+ D:/msy2/ucrt64/include/c++/15.2.0/ios \
+ D:/msy2/ucrt64/include/c++/15.2.0/iosfwd \
+ D:/msy2/ucrt64/include/c++/15.2.0/bits/stringfwd.h \
+ D:/msy2/ucrt64/include/c++/15.2.0/bits/memoryfwd.h \
+ D:/msy2/ucrt64/include/c++/15.2.0/bits/postypes.h \
+ D:/msy2/ucrt64/include/c++/15.2.0/cwchar D:/msy2/ucrt64/include/wchar.h \
+ D:/msy2/ucrt64/include/corecrt.h D:/msy2/ucrt64/include/_mingw.h \
+ D:/msy2/ucrt64/include/_mingw_mac.h \
+ D:/msy2/ucrt64/include/_mingw_secapi.h D:/msy2/ucrt64/include/vadefs.h \
+ D:/msy2/ucrt64/include/sdks/_mingw_ddk.h \
+ D:/msy2/ucrt64/include/corecrt_stdio_config.h \
+ D:/msy2/ucrt64/include/corecrt_wstdlib.h \
+ D:/msy2/ucrt64/include/corecrt_wctype.h \
+ D:/msy2/ucrt64/include/_mingw_off_t.h \
+ D:/msy2/ucrt64/include/_mingw_stat64.h \
+ D:/msy2/ucrt64/include/swprintf.inl \
+ D:/msy2/ucrt64/include/sec_api/wchar_s.h \
+ D:/msy2/ucrt64/include/c++/15.2.0/exception \
+ D:/msy2/ucrt64/include/c++/15.2.0/bits/exception.h \
+ D:/msy2/ucrt64/include/c++/15.2.0/bits/version.h \
+ D:/msy2/ucrt64/include/c++/15.2.0/bits/exception_ptr.h \
+ D:/msy2/ucrt64/include/c++/15.2.0/bits/exception_defines.h \
+ D:/msy2/ucrt64/include/c++/15.2.0/bits/cxxabi_init_exception.h \
+ D:/msy2/ucrt64/lib/gcc/x86_64-w64-mingw32/15.2.0/include/stddef.h \
+ D:/msy2/ucrt64/include/stddef.h D:/msy2/ucrt64/include/crtdefs.h \
+ D:/msy2/ucrt64/include/c++/15.2.0/typeinfo \
+ D:/msy2/ucrt64/include/c++/15.2.0/bits/hash_bytes.h \
+ D:/msy2/ucrt64/include/c++/15.2.0/new \
+ D:/msy2/ucrt64/include/c++/15.2.0/bits/move.h \
+ D:/msy2/ucrt64/include/c++/15.2.0/type_traits \
+ D:/msy2/ucrt64/include/c++/15.2.0/bits/nested_exception.h \
+ D:/msy2/ucrt64/include/c++/15.2.0/bits/char_traits.h \
+ D:/msy2/ucrt64/include/c++/15.2.0/bits/localefwd.h \
+ D:/msy2/ucrt64/include/c++/15.2.0/x86_64-w64-mingw32/bits/c++locale.h \
+ D:/msy2/ucrt64/include/c++/15.2.0/clocale \
+ D:/msy2/ucrt64/include/locale.h D:/msy2/ucrt64/include/stdio.h \
+ D:/msy2/ucrt64/include/sec_api/stdio_s.h \
+ D:/msy2/ucrt64/include/c++/15.2.0/cctype D:/msy2/ucrt64/include/ctype.h \
+ D:/msy2/ucrt64/include/c++/15.2.0/bits/ios_base.h \
+ D:/msy2/ucrt64/include/c++/15.2.0/ext/atomicity.h \
+ D:/msy2/ucrt64/include/c++/15.2.0/x86_64-w64-mingw32/bits/gthr.h \
+ D:/msy2/ucrt64/include/c++/15.2.0/x86_64-w64-mingw32/bits/gthr-default.h \
+ D:/msy2/ucrt64/include/pthread.h D:/msy2/ucrt64/include/errno.h \
+ D:/msy2/ucrt64/include/sys/types.h D:/msy2/ucrt64/include/process.h \
+ D:/msy2/ucrt64/include/corecrt_startup.h \
+ D:/msy2/ucrt64/lib/gcc/x86_64-w64-mingw32/15.2.0/include/limits.h \
+ D:/msy2/ucrt64/lib/gcc/x86_64-w64-mingw32/15.2.0/include/syslimits.h \
+ D:/msy2/ucrt64/include/limits.h D:/msy2/ucrt64/include/signal.h \
+ D:/msy2/ucrt64/include/pthread_signal.h D:/msy2/ucrt64/include/time.h \
+ D:/msy2/ucrt64/include/sys/timeb.h \
+ D:/msy2/ucrt64/include/sec_api/sys/timeb_s.h \
+ D:/msy2/ucrt64/include/_timeval.h D:/msy2/ucrt64/include/pthread_time.h \
+ D:/msy2/ucrt64/include/pthread_compat.h D:/msy2/ucrt64/include/sched.h \
+ D:/msy2/ucrt64/include/pthread_unistd.h \
+ D:/msy2/ucrt64/include/c++/15.2.0/x86_64-w64-mingw32/bits/atomic_word.h \
+ D:/msy2/ucrt64/include/c++/15.2.0/bits/locale_classes.h \
+ D:/msy2/ucrt64/include/c++/15.2.0/string \
+ D:/msy2/ucrt64/include/c++/15.2.0/bits/allocator.h \
+ D:/msy2/ucrt64/include/c++/15.2.0/x86_64-w64-mingw32/bits/c++allocator.h \
+ D:/msy2/ucrt64/include/c++/15.2.0/bits/new_allocator.h \
+ D:/msy2/ucrt64/include/c++/15.2.0/bits/functexcept.h \
+ D:/msy2/ucrt64/include/c++/15.2.0/bits/cpp_type_traits.h \
+ D:/msy2/ucrt64/include/c++/15.2.0/bits/ostream_insert.h \
+ D:/msy2/ucrt64/include/c++/15.2.0/bits/cxxabi_forced.h \
+ D:/msy2/ucrt64/include/c++/15.2.0/bits/stl_iterator_base_funcs.h \
+ D:/msy2/ucrt64/include/c++/15.2.0/bits/concept_check.h \
+ D:/msy2/ucrt64/include/c++/15.2.0/debug/assertions.h \
+ D:/msy2/ucrt64/include/c++/15.2.0/bits/stl_iterator_base_types.h \
+ D:/msy2/ucrt64/include/c++/15.2.0/bits/stl_iterator.h \
+ D:/msy2/ucrt64/include/c++/15.2.0/ext/type_traits.h \
+ D:/msy2/ucrt64/include/c++/15.2.0/bits/ptr_traits.h \
+ D:/msy2/ucrt64/include/c++/15.2.0/bits/stl_function.h \
+ D:/msy2/ucrt64/include/c++/15.2.0/backward/binders.h \
+ D:/msy2/ucrt64/include/c++/15.2.0/ext/numeric_traits.h \
+ D:/msy2/ucrt64/include/c++/15.2.0/bits/stl_algobase.h \
+ D:/msy2/ucrt64/include/c++/15.2.0/bits/stl_pair.h \
+ D:/msy2/ucrt64/include/c++/15.2.0/bits/utility.h \
+ D:/msy2/ucrt64/include/c++/15.2.0/debug/debug.h \
+ D:/msy2/ucrt64/include/c++/15.2.0/bits/predefined_ops.h \
+ D:/msy2/ucrt64/include/c++/15.2.0/bit \
+ D:/msy2/ucrt64/include/c++/15.2.0/concepts \
+ D:/msy2/ucrt64/include/c++/15.2.0/bits/refwrap.h \
+ D:/msy2/ucrt64/include/c++/15.2.0/bits/invoke.h \
+ D:/msy2/ucrt64/include/c++/15.2.0/bits/range_access.h \
+ D:/msy2/ucrt64/include/c++/15.2.0/initializer_list \
+ D:/msy2/ucrt64/include/c++/15.2.0/bits/basic_string.h \
+ D:/msy2/ucrt64/include/c++/15.2.0/ext/alloc_traits.h \
+ D:/msy2/ucrt64/include/c++/15.2.0/bits/alloc_traits.h \
+ D:/msy2/ucrt64/include/c++/15.2.0/bits/stl_construct.h \
+ D:/msy2/ucrt64/include/c++/15.2.0/string_view \
+ D:/msy2/ucrt64/include/c++/15.2.0/bits/functional_hash.h \
+ D:/msy2/ucrt64/include/c++/15.2.0/bits/string_view.tcc \
+ D:/msy2/ucrt64/include/c++/15.2.0/ext/string_conversions.h \
+ D:/msy2/ucrt64/include/c++/15.2.0/cstdlib \
+ D:/msy2/ucrt64/include/stdlib.h \
+ D:/msy2/ucrt64/include/sec_api/stdlib_s.h \
+ D:/msy2/ucrt64/include/c++/15.2.0/stdlib.h \
+ D:/msy2/ucrt64/include/malloc.h \
+ D:/msy2/ucrt64/lib/gcc/x86_64-w64-mingw32/15.2.0/include/mm_malloc.h \
+ D:/msy2/ucrt64/include/c++/15.2.0/bits/std_abs.h \
+ D:/msy2/ucrt64/include/c++/15.2.0/cstdio \
+ D:/msy2/ucrt64/include/c++/15.2.0/cerrno \
+ D:/msy2/ucrt64/include/c++/15.2.0/bits/charconv.h \
+ D:/msy2/ucrt64/include/c++/15.2.0/bits/basic_string.tcc \
+ D:/msy2/ucrt64/include/c++/15.2.0/bits/memory_resource.h \
+ D:/msy2/ucrt64/include/c++/15.2.0/cstddef \
+ D:/msy2/ucrt64/include/c++/15.2.0/bits/uses_allocator.h \
+ D:/msy2/ucrt64/include/c++/15.2.0/bits/uses_allocator_args.h \
+ D:/msy2/ucrt64/include/c++/15.2.0/tuple \
+ D:/msy2/ucrt64/include/c++/15.2.0/bits/locale_classes.tcc \
+ D:/msy2/ucrt64/include/c++/15.2.0/system_error \
+ D:/msy2/ucrt64/include/c++/15.2.0/x86_64-w64-mingw32/bits/error_constants.h \
+ D:/msy2/ucrt64/include/c++/15.2.0/stdexcept \
+ D:/msy2/ucrt64/include/c++/15.2.0/streambuf \
+ D:/msy2/ucrt64/include/c++/15.2.0/bits/streambuf.tcc \
+ D:/msy2/ucrt64/include/c++/15.2.0/bits/basic_ios.h \
+ D:/msy2/ucrt64/include/c++/15.2.0/bits/locale_facets.h \
+ D:/msy2/ucrt64/include/c++/15.2.0/cwctype \
+ D:/msy2/ucrt64/include/wctype.h \
+ D:/msy2/ucrt64/include/c++/15.2.0/x86_64-w64-mingw32/bits/ctype_base.h \
+ D:/msy2/ucrt64/include/c++/15.2.0/bits/streambuf_iterator.h \
+ D:/msy2/ucrt64/include/c++/15.2.0/x86_64-w64-mingw32/bits/ctype_inline.h \
+ D:/msy2/ucrt64/include/c++/15.2.0/bits/locale_facets.tcc \
+ D:/msy2/ucrt64/include/c++/15.2.0/bits/basic_ios.tcc \
+ D:/msy2/ucrt64/include/c++/15.2.0/bits/ostream.tcc \
+ D:/msy2/ucrt64/include/c++/15.2.0/istream \
+ D:/msy2/ucrt64/include/c++/15.2.0/bits/istream.tcc
